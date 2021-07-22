ab0249a7fd1d0575b5e7b93fdf43e526daa2b64b
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-22 14:00:57 -0400

    Added Replit folder into gitignore file

--- Replit/I/Basics/PrintVariable2.java
+++
@@ -1,9 +0,0 @@
-package I.Basics;
-
-public class PrintVariable2 {
-    //Given a String variable message write a statement to disply its value
-    public static void main(String[] args) {
-        String message = "I am a Cybertek school student.";
-        System.out.println(message);
-    }
-}
