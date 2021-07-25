02a5ea63cb3cae7a4b49c75074ea07aaa6a49be1
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-25 13:34:44 -0400

    made many changes in ArrayListMethods.java

--- src/day35_ArrayList/ArrayListMethods.java
+++ src/day35_ArrayList/ArrayListMethods.java
@@ -7,41 +7,58 @@ import java.util.Arrays;
 
 public class ArrayListMethods {
     public static void main(String[] args) {
-        ArrayList<String> groceryList = new ArrayList<>();
-        groceryList.add("Eggs");
-        groceryList.add("Milk");
-        groceryList.add("Water");
-        groceryList.add("Viniger");
-        groceryList.add("Bread");
 
-        System.out.println(groceryList);
+        System.out.println("********** add(Object) method ******************");
+
+        ArrayList<String> groceryList = new ArrayList<>();
+        groceryList.add("Eggs");    // 0
+        groceryList.add("Milk");    // 1
+        groceryList.add("Water");   // 2
+        groceryList.add("Viniger"); // 3
+        groceryList.add("Bread");   // 4
+
+                             // index         0     1     2       3        4
+        System.out.println(groceryList); //[Eggs, Milk, Water, Viniger, Bread]
+
+
+        System.out.println("\n*** add(index, Object): adds the element to the given index (index must be exist) ******\n");
+
+        groceryList.add(2,"Soda");
+                            // index         0     1     2       3        4     5
+        System.out.println(groceryList); //[Eggs, Milk, Soda, Water, Viniger, Bread]
+
+        System.out.println("\n****** set(index, Object): replaces the old element at given index with new element ********\n");
 
         groceryList.set(0, "Toilet Paper");
-        groceryList.set(3, "Soda");
-        System.out.println(groceryList);
+        groceryList.set(3, "Ayran");
+                               // index            0          1     2     3      4        5
+        System.out.println(groceryList);   //[Toilet Paper, Milk, Soda, Ayran, Viniger, Bread]
 
         boolean hasToiletPaper = groceryList.contains("Toilet Paper");
-        boolean hasToiletPaper2 = groceryList.indexOf("Toilet Paper") >=0 ;
+        boolean hasToiletPaper2 = groceryList.indexOf("Toilet Pape") >=0 ;
 
         System.out.println("hasToiletPaper = " + hasToiletPaper);
         System.out.println("hasToiletPaper2 = " + hasToiletPaper2);
 
 
-        System.out.println("********* remove() metod **************");
+        System.out.println("\n********* remove(index): removes the object at the given index ***********\n");
         //remove(index)
-        groceryList.remove(1);
-        System.out.println(groceryList.toString());
+        groceryList.remove(1); // 1, Milk
+                            //index                          0         1     2      3        4
+        System.out.println(groceryList.toString()); //[Toilet Paper, Soda, Ayran, Viniger, Bread]
+
         //remove(object)
-        boolean r1 = groceryList.remove("Water");
+        System.out.println("\n********* remove(object): removes the first matching object ***********\n");
+        boolean r1 = groceryList.remove("Water"); // false, because there is no object "Water" in the list
         System.out.println(groceryList);
         System.out.println("r1 = " + r1);
 
-        boolean r2 = groceryList.remove("Face Masks");
 
+        boolean r2 = groceryList.remove("Face Masks");
         System.out.println(groceryList);
         System.out.println("r2 = " + r2);
 
-        System.out.println("**************************************************");
+        System.out.println("********** new ArrayList is created *********************");
 
         ArrayList<Integer> list1 = new ArrayList<>();
         list1.add(10); //0
@@ -50,16 +67,26 @@ public class ArrayListMethods {
         list1.add(40);//3
         list1.add(50);//4
 
-        Integer a = 30;
-        list1.remove(a);
+        System.out.println("\n********* remove(object): removes the first matching object ***********\n");
+
         System.out.println(list1);
+
+        Integer a = 30;
+        list1.remove(a); // a is object here, that is why it is gonna remove first obejct matching with 30
+        System.out.println(list1);
+
+        System.out.println("\n******** 50 is object here, not index number because valueOf returns wrapper class value is object ****");
         list1.remove(Integer.valueOf(50));
         System.out.println(list1);
 
+        System.out.println("\n****** add(index, Object): adds the element to the given index (index must be exist) ******\n");
+
         list1.add(1, 3);
+        System.out.println(list1); //[10, 3, 20, 40]
+        list1.remove((Integer) 3); //this is gonna remove object named 3, not the index number 3 (40)
         System.out.println(list1);
-        list1.remove((Integer) 3); //this is gonna remove object named 3, not the index number 3
-        System.out.println(list1);
+
+        System.out.println("\n****** clear(): removes all the objects from the List *******\n");
 
         groceryList.clear();
         System.out.println("gorceryList = " + groceryList);
@@ -67,7 +94,7 @@ public class ArrayListMethods {
         list1.clear();
         System.out.println("list1 = " + list1);
 
-        System.out.println("******************************************************");
+        System.out.println("****************** list ArrayList is created *****************************");
 
         ArrayList<Character> list = new ArrayList<Character>();
         list.add('A');
@@ -80,8 +107,18 @@ public class ArrayListMethods {
 
         System.out.println(list);
 
+        System.out.println("******* indexOf(Object): returns the first matching object' index number (int)*********");
+
+
         list.indexOf('A');//0
         list.lastIndexOf('B');//3
+
+        System.out.println(list.indexOf('A'));// 0 bc A is the first object of List
+        System.out.println(list.indexOf('Z'));  // -1 because there is no Z in the list
+
+        System.out.println("**** unique charachters from list ArrayList******");
+        System.out.println("******* lastIndexOf(object): returns the last matching object' index number**********");
+
 
         ArrayList<Character> uniqueChars = new ArrayList<>();
 
@@ -105,7 +142,11 @@ public class ArrayListMethods {
 
         System.out.println(characters);
 
+        System.out.println("********* remove dublicated chars *************");
+
         ArrayList<Character> result = new ArrayList<>(); //[A, B, C]
+
+        System.out.println("**** contains(Object): checks if the object is contained in the list ***");
 
         for (int i = 0; i < characters.size(); i++) {
             if (!result.contains(characters.get(i))) {
@@ -114,7 +155,7 @@ public class ArrayListMethods {
         }
         System.out.println("result = " + result);
 
-        System.out.println("***********************************************************");
+        System.out.println("************** Reversing ArrayList *********************");
 
         ArrayList<Integer>  nums = new ArrayList<>();
 
@@ -133,7 +174,7 @@ public class ArrayListMethods {
 
         System.out.println(result2); //reversing result
 
-        System.out.println("*********************************************************************************");
+        System.out.println("************** equals(list): returns boolean ****************************");
 
         ArrayList<Integer> n1 = new ArrayList<>();
         n1.add(10);
@@ -142,6 +183,7 @@ public class ArrayListMethods {
         ArrayList<Integer> n2 = new ArrayList<>();
         n2.add(10);
         n2.add(20);
+        ;
 
         System.out.println(n1 == n2); // false
         System.out.println(n1.equals(n2)); // true
