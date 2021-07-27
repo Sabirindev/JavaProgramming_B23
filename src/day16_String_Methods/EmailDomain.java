b851b2b986ff48f5c69b4f25393659170ad57a7d
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-26 20:45:04 -0400

    made some changes in EmailDomain.java class in day16 folder

--- src/day16_String_Methods/EmailDomain.java
+++ src/day16_String_Methods/EmailDomain.java
@@ -3,12 +3,13 @@ package day16_String_Methods;
 public class EmailDomain {
     public static void main(String[] args) {
         String email = "Cybertek.computer@yahoo.com";
+        //index         0123456789012345678901234567
 
-
-        int beginningIndex = email.indexOf('@') + 1;
+        int beginningIndex = email.indexOf("@");// 17
+        System.out.println(beginningIndex);
         int endingIndex = email.lastIndexOf(".");
 
-        String domain = email.substring(beginningIndex,endingIndex);
+        String domain = email.substring(beginningIndex +1,endingIndex);
 
         System.out.println("Domain = "+domain); //gmail
 
