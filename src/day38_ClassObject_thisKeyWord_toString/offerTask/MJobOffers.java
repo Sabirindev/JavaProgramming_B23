4111836fa394a4ae263d78baaa84522710fb2b7a
sabir mustafayev <sdetinscrum@gmail.com>
2021-08-02 15:59:49 -0400

    created MJobOffers.java class

---
+++ src/day38_ClassObject_thisKeyWord_toString/offerTask/MJobOffers.java
@@ -0,0 +1,10 @@
+package day38_ClassObject_thisKeyWord_toString.offerTask;
+
+public class MJobOffers {
+
+    public static void main(String[] args) {
+        Offer myoffer1 = new Offer();
+
+    }
+
+}
