99b0023fafd442defdaf1eacc6f23157b09a2634
sabir mustafayev <sdetinscrum@gmail.com>
2021-08-02 15:17:57 -0400

    added some statementsinto BankAccount.java

--- src/day38_ClassObject_thisKeyWord_toString/BankAccount.java
+++ src/day38_ClassObject_thisKeyWord_toString/BankAccount.java
@@ -40,7 +40,7 @@ public class BankAccount {
     }
 
     public void checkBalance(){
-        System.out.println(accountHolder+"'s available balance is "+balance);
+        System.out.println(accountHolder+"'s available balance is $"+balance);
     }
 }
 /*
