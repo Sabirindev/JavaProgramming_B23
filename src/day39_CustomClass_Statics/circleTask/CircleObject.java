6b2db795cd8e1b416dbc65749fbe25490bbc9b98
sabir mustafayev <sdetinscrum@gmail.com>
2021-08-03 14:55:17 -0400

    created CircleObject.java class

---
+++ src/day39_CustomClass_Statics/circleTask/CircleObject.java
@@ -0,0 +1,10 @@
+package day39_CustomClass_Statics.circleTask;
+
+public class CircleObject {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        circle1.setInfo(2.5);
+
+        System.out.println(circle1);
+    }
+}
